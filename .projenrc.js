4ecb707e999e9ab729fc417be9ec938606c8fbaf
Ben Limmer <ben@benlimmer.com>
2023-06-11 12:10:45 -0600

    feat: publish python, dotnet packages Fixes #12

--- .projenrc.js
+++ .projenrc.js
@@ -7,9 +7,30 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: "2.59.0", // First release of 2023
   defaultReleaseBranch: "main",
   name: "@blimmer/cdk-static-wordpress",
+  description: "Generate a static site from Wordpress (via WP2Static) using AWS CDK",
   npmAccess: NpmAccess.PUBLIC,
   repositoryUrl: "https://github.com/blimmer/cdk-static-wordpress.git",
-  keywords: ["cdk", "aws-cdk", "aws-cdk-construct", "projen", "wordpress", "static-site"],
+  keywords: ["cdk", "aws-cdk", "aws-cdk-construct", "projen", "wordpress", "static-site", "wp2static"],
+
+  publishToNuget: {
+    nugetApiKeySecret: "NUGET_TOKEN",
+    dotNetNamespace: "BenLimmer.CdkStaticWordpress",
+    packageId: "BenLimmer.CdkStaticWordpress",
+  },
+
+  publishToPypi: {
+    twineUsernameSecret: "TWINE_USERNAME", // this resolves to __token__ to use token auth
+    twinePasswordSecret: "PYPI_TOKEN",
+    distName: "cdk-static-wordpress",
+    module: "cdk_static_wordpress",
+  },
+
+  publishToMaven: {
+    mavenRepositoryUrl: "https://maven.pkg.github.com/${{ github.repository }}",
+    javaPackage: "com.benlimmer.cdkstaticwordpress",
+    mavenGroupId: "com.benlimmer",
+    mavenArtifactId: "cdk-static-wordpress",
+  },
 
   docgen: true,
 
@@ -31,8 +52,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
 
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ["prettier-plugin-organize-imports"],
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
